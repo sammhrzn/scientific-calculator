603618839e05feb9a112a3504f725f9f27f6cea7
sammhrzn <maharjansamyak3@gmail.com>
2025-06-06 16:44:38 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -1,38 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () { });
 
-        const display = document.getElementById('calc-display');
-        const buttons = document.getElementsByClassName('btn');
+const display = document.getElementById('calc-display');
+const buttons = document.getElementsByClassName('btn');
 
-        let currentValue = "";
+let currentValue = "";
 
-        function evaluateResult() {
-            console.log('currentValue:', currentValue)
-            const convertedValue = currentValue
-            .replace("×","*")
-            .replace("÷","/")
-            .replace("%",'*0.01');
+function evaluateResult() {
+    console.log('currentValue:', currentValue)
+    const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace("%", '*0.01')
+        .replace('sin', 'Math.sin')
+        .replace('cos', 'Math.cos')
+        .replace('ln', 'Math.log')
+        .replace('π', 'Math.PI')
+        .replace('log', 'Math.log10')
+        .replace('e', 'Math.E')
+        .replace('tan', 'Math.tan')
+        .replace('√', 'Math.sqrt');
 
-            console.log('convertedValue:', convertedValue)
-            const result = eval(convertedValue);
-            currentValue = result.toString();
+    console.log('convertedValue:', convertedValue)
+    const result = eval(convertedValue);
+    currentValue = result.toString();
+    display.value = currentValue;
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function () {
+        const value = button.innerText;
+
+
+        try {
+            if (value == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+            } else if (value == "=") {
+                evaluateResult();
+            } else {
+                currentValue += value;
+                // console.log('curentValue:', currentValue);
+                display.value = currentValue;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            currentValue = "ERROR";
             display.value = currentValue;
         }
 
-        for (let i = 0; i < buttons.length; i++) {
-            const button = buttons[i];
-            button.addEventListener('click', function () {
-                const value = button.innerText;
 
-                if (value == "AC") {
-                    currentValue = "";
-                    display.value = currentValue;
-                } else if (value == "=") {
-                    evaluateResult();
-                } else {
-                    currentValue += value;
-                    // console.log('curentValue:', currentValue);
-                    display.value = currentValue;
-                }
-            })
+    })
 
-        }
+}
